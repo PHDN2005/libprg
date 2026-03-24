160fc3bb2c1ee84d34c068912dc94cc55321f5f0
pablo <pablo.hdn@aluno.ifsc.edu.br>
2026-03-24 17:27:26 -0300

    terminamos e implantamos 5 funções

--- libprg/src/libprg/pilha.c
+++ libprg/src/libprg/pilha.c
@@ -21,8 +21,35 @@ pilha_t* criar_pilha(int capacidade) {
   return pilha;
 }
 
-// empilhar
-// desempilhar
-// tamanho
-// vazia
-// destruir_pilha
+int empilhar(pilha_t* pilha, int valor) {
+
+  if (pilha->topo + 1 >= pilha->capacidade) {
+    pilha->capacidade *= 2;
+    pilha->elementos = realloc(pilha->elementos, sizeof(int) * pilha->capacidade);
+  }
+      pilha-> topo++;
+      pilha->elementos[pilha->topo] = valor;
+  return 0;
+}
+
+int desempilhar(pilha_t* pilha) {
+  if (pilha->topo < 0) {
+    exit(EXIT_FAILURE);
+  }
+  int valor = pilha->elementos[pilha->topo];
+  pilha->topo--;
+  return valor;
+}
+
+int tamanho(pilha_t* pilha) {
+  int N = pilha->capacidade;
+  return N;
+}
+
+int vazia(pilha_t* pilha) {return pilha->topo < 0;}
+
+int destruir_pilha(pilha_t* pilha) {
+  free(pilha->elementos);
+  free(pilha);
+  return 0;
+}
